4624d60f13ce135e44e80398221fa3bb71ccf317
Takrim Chowdhury <83010929+Takrimelahichowdhury@users.noreply.github.com>
2022-03-30 21:31:15 +0600

    Add files via upload

---
+++ Pair.cpp
@@ -0,0 +1,25 @@
+#include<bits\stdc++.h>
+using namespace std;
+int main()
+{
+
+    int n;
+    cin>>n;
+    for(int i=1; i<=n; i++)
+    {
+
+    pair<int,string>p;
+    cin>>p.first;
+    cin>>p.second;
+
+    cout<<p.first<<endl;
+    cout<<p.second<<endl;
+
+    }
+
+
+
+
+
+
+}
